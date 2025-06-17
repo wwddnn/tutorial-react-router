48b4759daa1e102ce4cfb9afe659aab1cc0009ac
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-16 23:30:42 -0300

    Nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-
-import './App.css'
-import { Link } from 'react-router-dom'
+import "./App.css"
+import { Link, Outlet } from "react-router-dom"
 
 function App() {
-
   return (
-    <nav 
-      style={{
-        borderBottom: "solid 1px",
-        paddingBottom: "1rem",
-      }}
-    >
-      <Link to="/invoices">Invoices</Link>  |{" "}
-      <Link to="/expenses">Expenses</Link>
-    </nav>
+    <>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Expenses from './routes/Expenses/index.tsx'
-import Invoices from './routes/Invoices/index.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Expenses from "./routes/Expenses/index.tsx";
+import Invoices from "./routes/Invoices/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} />
+        <Route path="/" element={<App />} >
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />} />
+        </Route>
     </Routes>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
