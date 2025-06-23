f094db1996343cf99fa6cc05e05126e7b5317945
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-22 21:42:09 -0300

    Reading URL Params

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Expenses from "./routes/Expenses/index.tsx";
 import Invoices from "./routes/Invoices/index.tsx";
 import NotFound from "./routes/NotFound/index.tsx";
+import Invoice from "./routes/Invoices/Invoice";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
