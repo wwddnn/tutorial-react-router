a41940dc1f2d4e46b64a335598b64a5f2646c09c
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-25 22:56:03 -0300

    Index routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import Expenses from "./routes/Expenses/index.tsx";
 import Invoices from "./routes/Invoices/index.tsx";
 import NotFound from "./routes/NotFound/index.tsx";
 import Invoice from "./routes/Invoices/Invoice";
+import InvoicesIndex from "./routes/Invoices/InvoicesIndex";
+import Welcome from "./routes/Welcome";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
+        <Route index element={<Welcome />} />
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route index element={<InvoicesIndex />} />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
         <Route path="*" element={<NotFound />} />
---
+++ src/routes/Welcome/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Welcome() {
+    return(
+        <main>
+            <p>Bem-vindo</p>
+        </main>
+    );
+}
